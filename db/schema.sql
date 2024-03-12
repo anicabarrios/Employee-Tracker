b35f1c860c4bc563abfb0f2cb73be7b81d518e2e
anicabarrios <anicabarrios1@gmail.com>
2024-03-12 18:07:45 -0400

    fixed sintax errors in schema.sql and seeds.sql files

--- db/schema.sql
+++ db/schema.sql
@@ -1,30 +1,34 @@
+-- Drop the database if exists
 DROP DATABASE IF EXISTS employee_db;
+
+-- Create the employee database
 CREATE DATABASE employee_db;
 
+-- Switch to the employee database
 USE employee_db;
 
-CREATE TABLE department(
+-- Create the department table
+CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) INT NOT NULL
+  name VARCHAR(30) NOT NULL
 );
-CREATE TABLE role(
+
+-- Create the role table
+CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(30) INT NOT NULL,
-  salary DECIMAL,
-  deparment_id INTERGER NOT NULL,
-  FOREIGN KEY (deparment_id)
-  REFERENCES department(id)
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10, 2) NOT NULL,
+  department_id INT NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
-CREATE TABLE employee(
+
+-- Create the employee table
+CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) INT NOT NULL,
-  last_name VARCHAR(30) INT NOT NULL,
-
-  role_id INTERGER NOT NULL,
-  FOREIGN KEY (role_id)
-  REFERENCES role(id)
-
-  manager_id INTERGER NOT NULL,
-  FOREIGN KEY (manager_id_id)
-  REFERENCES employee(id)
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+  FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
